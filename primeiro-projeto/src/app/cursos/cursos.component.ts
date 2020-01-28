a3c735608f8da6130d418633e1e5fbaad288ef88
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-01-28 13:44:48 -0300

    Criação de serviço
    Criação de serviço de cursos e testes com a diretiva *ngFor

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cursos',
-  templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  selector: "app-cursos",
+  templateUrl: "./cursos.component.html",
+  styleUrls: ["./cursos.component.css"]
 })
 export class CursosComponent implements OnInit {
-  constructor() {}
+  nomePortal: string;
+  cursos: string[] = ["Java", "Ext JS", "Angular"];
+
+  constructor() {
+    this.nomePortal = "http://loiane.training";
+  }
 
   ngOnInit() {}
 }
