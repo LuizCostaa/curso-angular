52e2fb8bed32e1d1418ebe08696c2c900088dfb3
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-02-03 14:48:42 -0300

    Criação do projeto de dada binding

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
 import { DetalheComponent } from './detalhe/detalhe.component';
+import { CursosService } from './cursos.service';
 
 @NgModule({
   declarations: [CursosComponent, DetalheComponent],
   imports: [CommonModule],
   exports: [
     CursosComponent
-  ]
+  ],
+  providers: [ CursosService ]
 })
 export class CursosModule { }
--- primeiro-projeto/src/app/cursos/cursos.service.ts
+++ primeiro-projeto/src/app/cursos/cursos.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 
-@Injectable({
-  providedIn: "root"
-})
+@Injectable()
 export class CursosService {
   constructor() {}
+
+  getCursos() {
+    return ["Java", "Ext JS", "Angular"];
+  }
 }
